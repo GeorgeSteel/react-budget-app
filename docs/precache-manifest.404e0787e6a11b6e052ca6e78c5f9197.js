d1bfd077bf3510e8b42c3eb26e4a230813efa45a
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2020-08-02 17:04:52 -0500

    Readme updated & production build uploaded

---
+++ docs/precache-manifest.404e0787e6a11b6e052ca6e78c5f9197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704dc6c8a2a1859ee4ad13670715809c",
+    "url": "/react-budget-app/index.html"
+  },
+  {
+    "revision": "5f37e4eecbbcf28c402e",
+    "url": "/react-budget-app/static/css/main.f5cd12b7.chunk.css"
+  },
+  {
+    "revision": "011a4ffd51f39b74fe78",
+    "url": "/react-budget-app/static/js/2.9ecc580a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-app/static/js/2.9ecc580a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f37e4eecbbcf28c402e",
+    "url": "/react-budget-app/static/js/main.65e0af23.chunk.js"
+  },
+  {
+    "revision": "bd3a7b93634578f10dc6",
+    "url": "/react-budget-app/static/js/runtime-main.9d0e09b1.js"
+  }
+]);
